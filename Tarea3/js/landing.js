c6c23e3e3f6945c322665ece23dd0b073ada14b7
dpasoss <dpasoss@ucenfotec.ac.cr>
2024-07-13 20:13:04 -0600

    Done

--- Tarea3/js/landing.js
+++ Tarea3/js/landing.js
@@ -14,3 +14,41 @@ document.addEventListener('DOMContentLoaded', function() {
         path: animationPath
     });
 });
+
+
+
+
+
+/*Boton de whatsapp*/
+    document.querySelector('.btn-whatsapp').addEventListener('click', function(event) {
+    });
+
+
+
+//desaparece el boton ScrollTop al cargar la pagina web
+document.getElementById("flechabtn").style.display = "none";
+
+//esconder botón ScrollTop (parte superior)
+window.onscroll = function() {
+    scrollFunction()
+};
+
+//funcion scroll
+function scrollFunction() {
+    if (document.body.scrollTop > 280 || document.documentElement.scrollTop > 280) {
+        document.getElementById("flechabtn").style.display = "block";
+    } else {
+        document.getElementById("flechabtn").style.display = "none";
+    }
+}
+
+
+
+
+//Funcián Scrolltop (Jquery)
+function topFunction() {
+    $('#flechabtn').click(function(event) {
+        event.preventDefault();
+        $('html, body').animate({ scrollTop: 0 }, 'slow');
+    });
+}
