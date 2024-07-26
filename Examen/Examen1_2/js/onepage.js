c0672294423e8dedb3a6ee9e73c1971499ee3a64
dpasoss <dpasoss@ucenfotec.ac.cr>
2024-07-26 13:02:59 -0600

    css

---
+++ Examen/Examen1_2/js/onepage.js
@@ -0,0 +1,22 @@
+// Inicializar WOW.js cuando el DOM esté completamente cargado
+document.addEventListener('DOMContentLoaded', function() {
+    // Inicializar WOW.js
+    new WOW().init();
+});
+
+
+
+    // Mostrar el botón cuando se desplaza hacia abajo
+    window.onscroll = function() {
+        var scrollToTopBtn = document.getElementById("flechabtn");
+        if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+            scrollToTopBtn.style.display = "block";
+        } else {
+            scrollToTopBtn.style.display = "none";
+        }
+    };
+
+    // Desplazarse hacia arriba cuando se hace clic en el botón
+    function topFunction() {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
